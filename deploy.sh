f5a0f5129feb37a0c09b857718d23ecc2d0f2956
Joey Cato <joeycato@gmail.com>
2018-01-06 16:52:41 -0800

    updated deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+rm -rf ./build
+wintersmith build
+cd ./build && \
+git init . && \
+git add . && \
+git commit -m "Deploy"; \
+git push "git@github.com:joeycato/joeycato.github.io.git" master --force && \
+rm -rf .git
